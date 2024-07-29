c43e11851db34afb902b09cdaab33f1718690afd
Pedro <pedrortgato@gmail.com>
2024-07-29 20:45:00 -0300

    Many to many association with JoinTable

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -15,7 +15,7 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_user")
+@Table(name = "tb_user") //cria o name da pasta do banco de dados
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
